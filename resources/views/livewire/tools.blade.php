2915ac5dfa2fdc780cb1ac7d20755a1479c31ab2
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-07-02 14:01:55 +0200

    Refine package

---
+++ resources/views/livewire/tools.blade.php
@@ -0,0 +1,12 @@
+<div wire:poll.1s>
+    <div class ="flex items-center gap-3">
+        @foreach ($tools as $tool)
+            <a href="{{ route($tool['route']) }}"
+                class="flex items-center gap-2 text-sm text-gray-700 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
+                target="_blank" rel="noopener noreferrer">
+                @include($tool['icon'])
+            </a>
+        @endforeach
+    </div>
+
+</div>
---
+++ src/Providers/HorizonServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Backstage\Tools\Providers;
+
+use Backstage\Tools\ToolsPlugin;
+use Filament\Facades\Filament;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+    }
+
+    /**
+     * Register the Horizon gate.
+     *
+     * This gate determines who can access Horizon in non-local environments.
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', function () {
+            return ToolsPlugin::get()->isAccessible();
+        });
+    }
+}
